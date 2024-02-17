bdfd596713be34f1d07ebb493c611aa4590dd963
Avishka Rathnakumara <a.rathnakumara777@gmail.com>
2024-02-17 15:47:43 +0530

    Update Instructions

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,39 @@
+# Python Calculator
+This is a simple Python calculator program that performs arithmetic operations such as addition, subtraction, multiplication, division, exponentiation, and finding the remainder. It also provides options to terminate the program or reset it.
+
+# Arithmetic Operations
+The following arithmetic operations are supported by the calculator:
+
+Addition (+)
+Subtraction (-)
+Multiplication (*)
+Division (/)
+Exponentiation (^)
+Remainder (%)
+
+# To use the calculator, follow these steps:
+
+Run the program.
+Select an operation by entering the corresponding symbol (+, -, *, /, ^, %).
+Enter the required numbers when prompted.
+The result of the operation will be displayed.
+You can terminate the program by entering '#' or reset it by entering '$'.
+
+# Example Usage
+Select operation.
+1.Add      : + 
+2.Subtract : - 
+3.Multiply : * 
+4.Divide   : / 
+5.Power    : ^ 
+6.Remainder: % 
+7.Terminate: # 
+8.Reset    : $ 
+Enter choice(+,-,*,/,^,%,#,$): +
++
+Enter first number: 10
+10
+Enter second number: 5
+5
+10.0 + 5.0 = 15.0
 
